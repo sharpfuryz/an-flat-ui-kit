eb407a90be97a5ce204f9e5c7ac888f7327a76a8
furyz <sharpfuryz@gmail.com>
2013-10-18 18:04:23 +0300

    Initial rails repo -- after all it will be public rubygem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnFlatUiKit::Application.config.secret_key_base = '958ed049e9eae1db1d2104960defaeb26047d4a64a3e9482f18bf60f1fa82446b7962e9ff7d099115230dd08cec30cead7ba88e89ed681071c7b057fb6852728'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnFlatUiKit::Application.config.session_store :cookie_store, key: '_an-flat-ui-kit_session'
