ae059f6daf47414f507f02f6f388aa1f9609a92d
furyz <sharpfuryz@gmail.com>
2013-10-18 18:39:45 +0300

    Scss flat buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+def google_web_font symbol
+  content_tag :link, '', {href: "http://fonts.googleapis.com/css?family=#{symbol}:400,700", rel: 'stylesheet', type: 'text/css'}
+end
 end
